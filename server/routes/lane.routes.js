f6f1cfc736a3dd7141ec82d0f21f6826fb0c33b0
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-04-25 23:13:41 +0200

    Add modules

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,9 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+router.route('/lanes').get(LaneController.getLanes);
+router.route('/lanes').post(LaneController.addLane);
+router.route('/lanes').post(LaneController.editLane);
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
 
 export default router;
